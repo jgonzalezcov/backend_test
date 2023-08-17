d00383a00cd728a6837774fcec357b274cbc148a
Juan Gonzalez Covarrubias <jgonzalezcov@gmail.com>
2023-08-17 12:47:36 -0400

    fix: crete variable portdb

--- src/db/connectionDb.js
+++ src/db/connectionDb.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     host: process.env.HOST,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: 5432,
+    port: process.env.PORTDB,
     allowExitOnIdle: true
 });
 

05d27c8a9ae695d8f4dc58f982303c4afc278b65
Juan Gonzalez Covarrubias <jgonzalezcov@gmail.com>
2023-11-28 19:00:40 -0300

    fix: Fixing error backend

--- src/controllers/TopicController.js
+++ src/controllers/TopicController.js
@@ -6,9 +6,9 @@ const TopicModel = require('../models/TopicModel');
 
 const list = async (req, res) => {
   try {
-    res.status(500).json({ msj: 'Ok  XDXDXDXD' });
-    // const response = await TopicModel.list();
-    // res.json(response);
+    // res.status(500).json({ msj: 'Ok  XDXDXDXD' });
+    const response = await TopicModel.list();
+    res.json(response);
   } catch (e) {
     console.log(e);
     res.status(500).json({ msj: 'Hola cagamos' });
